8f8c2a16dc65404680aeb74286f6d255a2393b36
Betül <Betl@DESKTOP-82FEO9D>
2026-04-09 23:49:29 +0300

    Proje dosyası ekle.

---
+++ Kutuphane/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using KutuphaneProjesi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace KutuphaneProjesi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; } // Veri tabanında Books tablosu oluşsun dedik
+    }
+}
---
+++ Kutuphane/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace KutuphaneProjesi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; } // Kitabın numarası (otomatik artar)
+        public string Title { get; set; } // Kitap Adı
+        public string Author { get; set; } // Yazarı
+        public decimal Price { get; set; } // Fiyatı
+        public int Stock { get; set; } // Stok Adedi
+    }
+}
